ac0d0f9c7fed446761dc39256442ae16b2355380
jak1841 <jaskarnd16@gmail.com>
2019-12-24 16:10:34 -0600

    Random number generator added

--- AllGIts/passwordGen.py
+++ AllGIts/passwordGen.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python 
+import random
 
 print("Hello world")
+
+#Random Function with upper and lower
+def randomgenerator(lowerbound,upperbound):
+ rnum = random.randint(lowerbound,upperbound)
+ return rnum
+
+for x in range (26):
+ print(randomgenerator(1,26))
